e7a688828d81aaba92ea796db310959385178c36
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-08-27 01:33:21 -0300

    Solução Matemática no Algoritimo

---
+++ Algebra.pas
@@ -0,0 +1,19 @@
+Program Algebraa;
+
+function Calcular(a,b,c,d,e,f : integer ):real
+ begin
+  read(a);
+  read(b);
+  read(c);
+  read(d);
+  read(e);
+  read(f);
+  end;
+var a1, b1, c1, d1, e1, f1 : integer;
+
+begin
+ readln(a1,b1,c1,d1,e1,f1);
+ Calcular(a1,b1,c1,d1,e1,f1);
+end.
+
+
