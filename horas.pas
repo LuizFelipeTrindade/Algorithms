470ac38f78e3ab4d0fc5b7b1bd1fb0f0dafafa8c
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-03 19:38:13 -0300

    Modificação de conteudo

--- horas.pas
+++ horas.pas
@@ -1,6 +1,6 @@
 Program Horas;
 
-var a, b, c, d : integer;
+var a, b, c, d, e, f : integer;
 
 begin
  write('Digite um numero menor que 23 para horas e menor que 59 para minutos: ');
@@ -9,11 +9,13 @@ begin
  write('Digite novamente: ');
  read(c, d);
 
- 
+ e := a + c;
+ f := b + d;
 
- if (a < 23) and (b < 59) then
-    write(a, ':', b, ' + ', c, ':', d)
+ if (a < 23) and (b < 59) and (e < 23) and (f < 59)  then
+    write(a, ':', b, ' + ', c, ':', d, ' = ', e, ':',f)
  else
-    write('Numero invalido');
+    write('Numero invalido, insira um numero em que a soma das horas não fique maior que 23, e a soma dos minutos maior que 59');
+
 end.
 
