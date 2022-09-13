2d0557d267a6c9c28d29f867f35bf9e76a1f1763
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-12 23:06:27 -0300

    Algoritimo que tem como funcionalidade determinar se um número que é lido pelo programa é par ou ímpar usando desvio condicional (if, else)

---
+++ Parimp.pas
@@ -0,0 +1,22 @@
+Program parorimp;
+
+
+var num : integer;
+
+
+begin
+
+write('Digite um numero: ');
+
+read(num);
+
+if num mod 2 = 1 then //Se o resto da divisao ser um, então...
+  
+  writeln('impar')
+
+else
+
+  writeln('Par');
+
+end.
+
