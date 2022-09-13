c43dab107e9e14fdbc7e752ef22a35f535e1aef9
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-13 20:01:06 -0300

    Algoritimo que lê uma variavel e determina seu antecessor e sucessor

---
+++ maiormenor.pas
@@ -0,0 +1,27 @@
+Program maioroumenor;
+
+
+var x, y, z : integer;
+
+
+begin
+ 
+ write('Digite um numero: ');
+ read(x);
+
+ write('Digite outro: ');
+ read(y);
+
+ write('Digite mais um pela ultima vez: ');
+ read(z);
+
+
+ if(x > y) or (y < z) then
+
+  writeln('SIM')
+
+ else
+   
+  writeln('NAO');
+
+end.
