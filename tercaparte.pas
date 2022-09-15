eac4bac8bce48d7d02ae35fa283a43ecbc5816a8
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-15 17:30:49 -0300

    Algoritimo que demonstra a terceira parte de um número

---
+++ tercaparte.pas
@@ -0,0 +1,17 @@
+Program terca;
+
+
+var a, res : real; //variavel a é de leitura, e a res é de exibição do resultado
+
+begin
+
+write('Digite um numero real, e em seguida sera impresso a terca parte desse numero: ');
+
+read(a);
+
+
+res := a / 3;
+
+write(res:0:2);
+
+end.
