f8f7e6d1de410a5de4c41abdae216d31be2c2b95
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-15 17:28:38 -0300

    Algoritimo que soma a media dos dois valores de uma variavel

---
+++ media.pas
@@ -0,0 +1,19 @@
+Program mediaaritimetica;
+
+
+var a, b, r : real;
+
+
+begin
+
+ write('Digite dois valores reais, e sera calculado a media desses valores: ');
+
+ read(a,b);
+
+ r := (a + b) / 2;
+
+ writeln(r:0:2);
+
+end.
+
+
