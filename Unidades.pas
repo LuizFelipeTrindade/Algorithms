696eb39503cf5ed13fd59d0ee2484af61b56489b
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-16 09:22:16 -0300

    Algoritimo que demonstra que é Unidade de quatro algarismos que compõe uma placa

---
+++ Unidades.pas
@@ -0,0 +1,15 @@
+Program un;
+
+var m, c, d, u : integer;
+
+
+begin
+ 
+ write('Digite quatro algarismos para compor a placa de um carro: ');
+ read(m,c,d,u);
+
+ writeln(m,'', c,'', '', d,'', u);
+
+ write('A unidade dessa placa eh: ', u);
+
+end.
