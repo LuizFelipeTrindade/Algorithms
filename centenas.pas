86719f92d7e62fd5f2a773c84297f2de0da741b2
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-18 21:08:12 -0300

    Algoritimo que lê a casa das centenas de um compostos de números de uma placa de carro

---
+++ centenas.pas
@@ -0,0 +1,16 @@
+Program cen;
+
+var m,c,d,u : integer;
+
+
+begin
+
+ write('Digite quatro algarismos para compor a placa de um carro: ');
+ read(m,c,d,u);
+
+ writeln(m,c,d,u);
+
+ write('O numero que corresponde a centena eh: ',c );
+
+end.
+
