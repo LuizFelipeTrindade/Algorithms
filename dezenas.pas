4584ab51a7ff38dfd1e25dad764c46e0c70defde
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-18 21:08:57 -0300

    Algoritimo que lê a casa das dezenas de um composto de números de uma placada de carro

---
+++ dezenas.pas
@@ -0,0 +1,14 @@
+Program deze;
+
+var m, c, d, u: integer;
+
+begin
+
+ write('Digite quatro algarismos para compor a placa de um carro: ');
+ read(m,c,d,u);
+
+ writeln(m,c,d,u);
+
+ write('O algarismo das dezenas eh: ', d);
+
+end.
