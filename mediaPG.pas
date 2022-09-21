65ab5357b0b6e5d0c84861fa251aac16b87aa990
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-21 17:47:52 -0300

    Algoritimo que calcula os valores, multiplicando duas variaveis e tirando a raiz do resultado da multiplicação, fazendo assim uma média geométrica

---
+++ mediaPG.pas
@@ -0,0 +1,21 @@
+Program media;
+
+
+var a, b, c, resultado : real;
+
+begin
+ 
+ write('Digite um numero: ');
+ read(a);
+
+ write('Digite outro: ');
+ read(c);
+
+ b := a * c ;
+  
+ resultado:= sqrt(b);
+
+ write(resultado:0:2);
+
+
+end.
