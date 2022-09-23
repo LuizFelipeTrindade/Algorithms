67f19e6321e9548ca15565b7172dda54129bcfc9
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-09-23 19:05:16 -0300

    Algoritimo que procura a razão de uma Progressão aritimética

---
+++ PArazao.pas
@@ -0,0 +1,19 @@
+Program razao;
+
+
+var n, a1, an : integer;
+
+begin
+
+ write('Adicione o primeiro termo de uma PA: ');
+ read(a1);
+
+ write('Adicione o enesimo termo da PA: ');
+ read(an);
+
+ write('Determine o N da PA: ');
+ read(n);
+  
+ writeln((n + a1) * 50);
+  
+end.
