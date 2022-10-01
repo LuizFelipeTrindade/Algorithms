09b98e1a81a1d76ac97ff825084e5280fbd5dced
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-10-01 01:13:34 -0300

    Algoritimo que faz a somatoria de dois valores, e através de estrutura condicional, determina se eles são maiores que dez, se sim, imprime o primeiro valor, se não, imprime o segundo valor

---
+++ somatoria.pas
@@ -0,0 +1,24 @@
+Program somatoriaa;
+
+var num1, num2, soma : integer;
+
+
+begin
+
+ write('Digite um valor: ');
+ read(num1);
+
+ write('Digite outro: ');
+ read(num2);
+
+ soma := num1 + num2;
+
+ if soma > 10 then
+   
+    write(num1)
+ 
+ else
+  
+    write(num2);
+
+end.
