7d821d70f3b458791173817e8a9a74260f5823bc
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-10-02 13:20:40 -0300

    Algoritimo que determina se um número é divisivel ou não por 3 e 7

---
+++ divisivel.pas
@@ -0,0 +1,18 @@
+Program divi;
+
+var num1 : integer;
+
+begin
+
+ write('Digite um numero: ');
+ read(num1);
+
+ if num1 mod 5 = 0 then
+ 
+  writeln('sim')
+
+ else
+  
+  writeln('nao');
+
+end. 
