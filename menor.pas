c6e87822dd481adecd40c512ed2d8f66233bd7b7
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-10-02 13:24:02 -0300

    Algoritimo que tem como função demosntrar se um número lido é menor do que o outro. Se sim, imprime o primeiro valor, se não, imprimer o segundo

---
+++ menor.pas
@@ -0,0 +1,21 @@
+Program men;
+
+var num1, num2 : real;
+
+begin
+ 
+ write('Digite um numero: ');
+ read(num1);
+ 
+ write('Digite outro: ');
+ read(num2);
+
+ if num1 > num2 then
+	
+  writeln(num2:0:2)
+
+ else if num2 > num1 then
+	
+  write(num1:0:2);
+
+end.
