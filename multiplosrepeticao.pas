2619558d599a51ddcbaca70cbf667b33f2dfcc11
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-10-16 01:49:40 -0300

    Algoritimo que faz a leitura de vários valores, e determina através de uma condição se os números lidos são múltiplos de 7 mas não de 2

---
+++ multiplosrepeticao.pas
@@ -0,0 +1,19 @@
+Program repeticao_de_multiplos;
+
+var multiplo, num : integer;
+
+begin
+
+ multiplo := 0;	//Multiplo começa com valor atribuido para não ter erro o programa
+ num := 1; 
+
+  while num <> 0 do begin
+    readln(num);
+
+    if (num mod 7 = 0) and (num mod 2 = 1) then begin
+	 multiplo := num;
+	 writeln('Eh multiplo de 7 e <> 2: ', multiplo); //Imprime na tela se a condição for verdadeira
+     end;
+  end;
+end.
+
