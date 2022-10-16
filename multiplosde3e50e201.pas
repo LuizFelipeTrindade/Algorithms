6480a9f607b09f626da6c79ce35669736b1828ba
Luiz Felipe <luizfelipe_14castro@hotmail.com>
2022-10-16 02:22:15 -0300

    Adicionando comentários para que fique mais espécifico as funções que o Algoritimo possui

--- multiplosde3e50e201.pas
+++ multiplosde3e50e201.pas
@@ -4,16 +4,16 @@ var num, multiplo: integer;
 
 begin
 
- num := 1;
+ num := 1; //Atribuição de um valor para não dar erro no programa
  multiplo := 0;
 
- while num <> 0 do begin
+ while num <> 0 do begin //Inicio da repetição
    readln(num);
 
    if (num mod 3 = 0) and (num > 50) and (num <= 201) then
    begin
-	multiplo := num;
-        writeln('Cumpre os requisitos: ', multiplo);
+	multiplo := num; //Atribuição de variavel dos requisitos acima
+        writeln('Cumpre os requisitos: ', multiplo); 
    end;
  end;
 end. 
