6470893002a638595ab66be5c87c2945c23d481c
Luiz <luizfelipe_14castro@hotmail.com>
2022-11-07 19:07:33 -0300

    Correção de erros no algoritimo, alterando a condição para as variáveis serem corrigidas durante o programa e adicionando comentários da funcionalidade

--- maiorm.pas
+++ maiorm.pas
@@ -1,27 +1,26 @@
-Program impressao_maior;
-
-var maior1, maior2, num : integer;
-
-begin
-  write('Digite varios valores em ordem crescente: ');
-  readln(num);
-
-  maior1:= num;
-  maior2:= num;
-
-  if num <> 0 then begin
+-Program impressao_maior;
+-
+-var maior1, maior2, num : integer;
+-
+-begin
+-  write('Digite varios valores em ordem CRESCENTE, do menor para o maior: ');
+-  readln(num);
+-
+-  maior1:= num;
+-  maior2:= num;
+-
+-  if num <> 0 then begin
+- 
+-    while num <> 0 do begin
+-     if (num >= maior1)  then begin //Faz a correção se o maior valor não está errado
+-       maior2 := maior1;            
+-       maior1 := num;
+-     end;
+-
+-      readln(num);
+-    end;
+-   writeln(maior1);
+-   writeln(maior2);
+-  end;
+-end.
  
-    while num <> 0 do begin
-     if num > maior1 then
-     maior2 := maior1;
-     maior1 := num;
-
-      readln(num);
-
-    end;
-   writeln(maior1);
-   writeln(maior2);
-  end;
-end.
-     
-   
